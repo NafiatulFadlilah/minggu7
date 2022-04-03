f32f11fff54acb0f5dbdaff3102fc031ec7c81dd
Nafiatul Fadlilah <nfdnafiatulfa@gmail.com>
2022-04-03 23:42:49 +0700

    Submit Jobsheet 7 - Nafiatul Fadlilah

---
+++ postForm.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+    </head>
+    <body>
+        Welcome <?php echo $_POST["username"]; ?> !!<br>
+        Your email address is: <?phpecho $_POST["email"]; ?>
+    </body>
+</html>
